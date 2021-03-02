07702c2a2f7b0a9d204d13fe795f51394ed7e11e
Mikey P <peraram2@gmail.com>
2021-03-01 18:13:01 -0800

    cases if name or id not string and id NAN

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -27,9 +27,23 @@ describe('Employee', () => {
 
             expect(cb).toThrowError(err);
         });
-        it("should throw and error if 'name' is not a string", );
-        it("should throw an error if 'id' is not a number");
-        it("should throw an error if 'email' is not a string.");
+        it("should throw and error if 'name' is not a string", () => {
+            const cb = () => new Employee(1024, 3062, 'email@example.com');
+            const err = new Error("Expected parameter 'name' to be a non-empty string");
 
+            expect(cb).toThrowError(err);
+        });
+        it("should throw an error if 'id' is not a number", () => {
+            const cb = () => new Employee('Luis', "Frank", 'email@example.com');
+            const err = new Error("Expected parameter 'id' to be a number");
+
+            expect(cb).toThrowError(err);
+        });
+        it("should throw an error if 'email' is not a string.", () => {
+            const cb = () => new Employee('Juana', 3062, 2563);
+            const err = new Error("Expected parameter 'email' to be a non-empty string");
+
+            expect(cb).toThrowError(err);
+        });
     });
 });
